c3e3844eda0e359923aea27abc40fc5126301f4d
jswildcards <eggkuv35c@relay.firefox.com>
2023-07-18 18:57:24 -0400

    + add routes, router, store

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,19 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import { createPinia } from 'pinia'
+
+import routes from './routes'
+
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const router = createRouter({
+  routes,
+  history: createWebHistory(import.meta.env.BASE_URL),
+})
+const store = createPinia()
+const app = createApp(App)
+
+app.use(router)
+app.use(store)
+app.mount('#app')
