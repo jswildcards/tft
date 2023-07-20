4beddbc8a4129200ebd1408c1fbf1691711a2df7
jswildcards <eggkuv35c@relay.firefox.com>
2023-07-20 01:21:01 -0400

    * update page layout

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,29 @@
+<script setup lang="ts">
+import Header from './components/Header.vue'
+</script>
+
 <template>
-  <router-link to="/">TFT Fan</router-link>
-  <router-view></router-view>
+  <div class="page-container">
+    <div class="header-container">
+      <Header />
+    </div>
+
+    <router-view class="main-container" />
+  </div>
 </template>
+
+<style scoped>
+.page-container {
+  @apply flex flex-col min-h-screen bg-slate-950 text-primary;
+}
+
+.header-container {
+  @apply bg-slate-900 shadow;
+}
+
+.main-container {
+  @apply container mx-auto mb-16 p-4 grow;
+  /* FIX: unexpected appearance showing different colors in background */
+  @apply border border-slate-950;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import SquareImage from './SquareImage.vue'
+</script>
+
+<template>
+  <div class="header">
+    <router-link to="/" class="header__btn-link">
+      <SquareImage src="/images/logo.png" size="sm" />
+      <span>TFT Fan</span>
+    </router-link>
+  </div>
+</template>
+
+<style scoped>
+.header {
+  @apply container mx-auto flex;
+}
+
+.header__btn-link {
+  @apply inline-flex items-center p-4 duration-100 hover:bg-slate-700;
+}
+
+.header__btn-link > * {
+  @apply mr-2;
+}
+
+.header__btn-link > *:last-child {
+  @apply mr-0;
+}
+</style>
