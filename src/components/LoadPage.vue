02497ab4ed955b3137a33e8c140a8e02aa8ff462
jswildcards <eggkuv35c@relay.firefox.com>
2023-09-14 21:07:42 -0400

    + add component LoadPage

---
+++ src/components/LoadPage.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { computed, ref, toRefs } from 'vue'
+
+interface Props {
+  isLoaded: boolean
+}
+
+const props = defineProps<Props>()
+
+const { isLoaded } = toRefs(props)
+</script>
+
+<template>
+  <div v-if="isLoaded">
+    <slot />
+  </div>
+  <div v-else class="loading">
+    <img src="/images/loading.svg" width="48" />
+    <div class="loading__text">Loading...</div>
+  </div>
+</template>
+
+<style scoped>
+.loading {
+  @apply flex flex-col justify-center items-center;
+}
+
+.loading__text {
+  @apply mt-1;
+}
+</style>
