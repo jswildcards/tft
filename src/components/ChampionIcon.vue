63dc9e38ac59ca814485bcdcff56ad8156cf3ab8
jswildcards <eggkuv35c@relay.firefox.com>
2023-09-14 20:30:35 -0400

    + add component ChampionIcon

---
+++ src/components/ChampionIcon.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { computed, ref, toRefs } from 'vue'
+
+import SquareImage from '../components/SquareImage.vue'
+
+interface Props {
+  src: string
+  cost: number
+  size?: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  size: "lg"
+})
+
+const { src, cost } = toRefs(props)
+</script>
+
+<template>
+  <SquareImage :size="size" :src="src" :class="`champion-icon champion-icon--cost-${cost}`" />
+</template>
+
+<style scoped>
+.champion-icon {
+  @apply rounded-full border-4;
+}
+
+.champion-icon--cost-1 {
+  @apply border-gray-500;
+}
+
+.champion-icon--cost-2 {
+  @apply border-green-500;
+}
+
+.champion-icon--cost-3 {
+  @apply border-blue-500;
+}
+
+.champion-icon--cost-4 {
+  @apply border-fuchsia-500;
+}
+
+.champion-icon--cost-5 {
+  @apply border-yellow-500;
+}
+</style>
